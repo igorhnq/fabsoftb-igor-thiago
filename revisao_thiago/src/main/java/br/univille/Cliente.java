8d68eafc15c859b63bc93f44d25269e4409b5d53
Thiago Schulz da Rosa <thiagoschulz.rosa@gmail.com>
2025-03-10 21:59:53 -0300

    feat: revisao

---
+++ revisao_thiago/src/main/java/br/univille/Cliente.java
@@ -0,0 +1,14 @@
+package br.univille;
+
+public class Cliente {
+    // Variavel, atributo vv
+    private String nome;
+    //Método vv (ou função)
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
