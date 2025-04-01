328afdb540baace8267c2728daa387cd2157caf3
THIAGO SCHULZ DA ROSA <thiago.rosa23@univille.br>
2025-04-01 20:40:18 -0300

    feat: Incrementando o Spring Boot

---
+++ projfabricasoftkaffeeleganz/src/main/java/br/univille/projfabricasoftkaffeeleganz/ProjfabricasoftkaffeeleganzApplication.java
@@ -0,0 +1,13 @@
+package br.univille.projfabricasoftkaffeeleganz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjfabricasoftkaffeeleganzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjfabricasoftkaffeeleganzApplication.class, args);
+	}
+
+}
