f82b66f6ad5c749c92f6f8f53b8b16c628a56545
igorhnq <igorhenrique102@gmail.com>
2025-04-20 17:32:50 -0300

    feat: estrutura inicial do projeto

---
+++ kaffeeleganz/src/main/java/com/kaffeeleganz/KaffeeleganzApplication.java
@@ -0,0 +1,13 @@
+package com.kaffeeleganz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaffeeleganzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaffeeleganzApplication.class, args);
+	}
+
+}
---
+++ kaffeeleganz/src/test/java/com/kaffeeleganz/KaffeeleganzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaffeeleganz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaffeeleganzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
