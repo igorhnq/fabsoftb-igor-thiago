86f90023c1712fffdfeedee3be71a1e3c75a796b
igorhnq <igorhenrique102@gmail.com>
2025-04-21 02:12:52 -0300

    fix: corrige a margin da página de produtos

--- frontend/src/pages/Products.tsx
+++ frontend/src/pages/Products.tsx
@@ -1,9 +1,11 @@
 import Banner from "../components/Banner/Banner";
+import CustomSelect from "../components/Input/Select/CustomSelect";
 
 export default function Products() {
     return (
-        <div>
+        <div style={{ margin: '0 5rem' }}>
             <Banner />
+            <CustomSelect />
         </div>
     );
 }
