febac53b96518db367baa0465d736dec0ab85c3f
igorhnq <igorhenrique102@gmail.com>
2025-04-21 18:05:48 -0300

    feat: adiciona carrosel de itens funcional

--- frontend/src/components/ProductCarousel/ProductCarousel.tsx
+++ frontend/src/components/ProductCarousel/ProductCarousel.tsx
@@ -1,13 +1,33 @@
+import { useKeenSlider } from "keen-slider/react";
 import ProductCard from "../Card/ProductCard/ProductCard";
 
+import "keen-slider/keen-slider.min.css";
 import styles from "./ProductCarousel.module.css";
 
 export default function ProductCarousel() {
+    const [sliderRef, instanceRef] = useKeenSlider({
+        slides: { perView: 5, spacing: 20 },
+    });
+
     return (
-        <div className={styles.productCarousel}>
-            <h4>Produtos</h4>
-            <div className={styles.productCarouselContainer}>
-                <ProductCard />
+        <div>
+            <h4>Promoções</h4>
+            <div className={styles.carouselWrapper}>
+                <button onClick={() => instanceRef.current?.prev()} className={styles.arrowLeft}>◀</button>
+
+                <div ref={sliderRef} className="keen-slider">
+                    <div className="keen-slider__slide"><ProductCard /></div>
+                    <div className="keen-slider__slide"><ProductCard /></div>
+                    <div className="keen-slider__slide"><ProductCard /></div>
+                    <div className="keen-slider__slide"><ProductCard /></div>
+                    <div className="keen-slider__slide"><ProductCard /></div>
+                    <div className="keen-slider__slide"><ProductCard /></div>
+                    <div className="keen-slider__slide"><ProductCard /></div>
+                    <div className="keen-slider__slide"><ProductCard /></div>
+                    <div className="keen-slider__slide"><ProductCard /></div>
+                </div>
+
+                <button onClick={() => instanceRef.current?.next()} className={styles.arrowRight}>▶</button>
             </div>
         </div>
     );
