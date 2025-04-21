dc494c85cfeb68e9ad4153daa842fd5cc8533dea
igorhnq <igorhenrique102@gmail.com>
2025-04-21 14:54:45 -0300

    feat: cria card de produto

---
+++ frontend/src/components/Card/ProductCard/ProductCard.tsx
@@ -0,0 +1,9 @@
+import styles from "./ProductCard.module.css";
+
+export default function ProductCard() {
+    return (
+        <div className={styles.productCard}>
+            
+        </div>
+    );
+}
