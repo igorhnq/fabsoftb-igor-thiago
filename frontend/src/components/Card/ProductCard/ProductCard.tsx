bf8d3d79d51d25fd6d6091b45a7d6431b45e20f1
igorhnq <igorhenrique102@gmail.com>
2025-04-21 15:05:07 -0300

    fix: corrige estilos do card de produto

--- frontend/src/components/Card/ProductCard/ProductCard.tsx
+++ frontend/src/components/Card/ProductCard/ProductCard.tsx
@@ -3,7 +3,8 @@ import styles from "./ProductCard.module.css";
 export default function ProductCard() {
     return (
         <div className={styles.productCard}>
-            
+            <div className={styles.priceTag}>R$ 00,00</div>
+            <div className={styles.productName}>Café Americano</div>
         </div>
     );
 }
