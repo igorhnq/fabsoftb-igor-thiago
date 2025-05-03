91abe954527ff5319b938cb3558df7ee9345aaaf
igorhnq <igorhenrique102@gmail.com>
2025-05-03 15:43:41 -0300

    feat: adiciona propriedade isTransparent pro header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -4,20 +4,22 @@ import logo from "../../assets/logo.svg";
 
 import styles from "./Header.module.css";
 
-export default function Header() {
+export default function Header({ isTransparent = false }) {
     return (
-        <header>
+        <header className={isTransparent ? styles.transparentHeader : ''}>
             <img src={logo} />
-            <nav>
-                <ul className={styles.navList}>
-                    <li><Link className={styles.link} to="/">Início</Link></li>
-                    <li><Link className={styles.link} to="/products">Cardápio</Link></li>
-                    <li><Link className={styles.link} to="/cafeterias">Cafeterias</Link></li>
-                    <li><Link className={styles.link} to="/franquiador">Franquiador</Link></li>
-                </ul>
-                <Bag className={styles.icon} size={32} weight="bold" color="var(--matcha-leaf)" />
-                <User className={styles.icon} size={32} weight="bold" color="var(--matcha-leaf)" />
-            </nav>
+            {!isTransparent && (
+                <nav>
+                    <ul className={styles.navList}>
+                        <li><Link className={styles.link} to="/">Início</Link></li>
+                        <li><Link className={styles.link} to="/products">Cardápio</Link></li>
+                        <li><Link className={styles.link} to="/cafeterias">Cafeterias</Link></li>
+                        <li><Link className={styles.link} to="/franquiador">Franquiador</Link></li>
+                    </ul>
+                    <Bag className={styles.icon} size={32} weight="bold" color="var(--matcha-leaf)" />
+                    <User className={styles.icon} size={32} weight="bold" color="var(--matcha-leaf)" />
+                </nav>
+            )}
         </header>
     );
 }
