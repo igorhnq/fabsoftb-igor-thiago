18127bcdccd8a0bff3ed93654c3a46d8f98dbe1f
igorhnq <igorhenrique102@gmail.com>
2025-05-03 15:42:35 -0300

    feat: adiciona novas propriedades customizáveis

--- frontend/src/components/Input/Button/Button.tsx
+++ frontend/src/components/Input/Button/Button.tsx
@@ -2,11 +2,24 @@ import styles from "./Button.module.css";
 
 interface ButtonProps {
     label: string;
+    isTransparent?: boolean;
+    backgroundColor?: string;
+    color?: string;
+    fontWeight?: string;
 }
 
-export default function Button({ label }: ButtonProps) {
+export default function Button({ label, isTransparent = false, backgroundColor, color, fontWeight }: ButtonProps) {
+    const buttonStyle = {
+        backgroundColor: isTransparent ? 'transparent' : `var(${backgroundColor})` || 'var(--matcha-moss)',
+        color: isTransparent ? `var(${color})` || 'var(--matcha-moss)' : `var(${color})` || 'var(--vanilla-cream)',
+        fontWeight: fontWeight || '500',
+    };
+
     return (
-        <button className={styles.button}>
+        <button 
+            className={styles.button}
+            style={buttonStyle}
+        >
             {label}
         </button>
     );
