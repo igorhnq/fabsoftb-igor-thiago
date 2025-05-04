5ac0aaf43110e325ee595e118b46c44d158a9348
igorhnq <igorhenrique102@gmail.com>
2025-05-04 18:02:31 -0300

    fix: remove campos inutilizados da tabela user

--- kaffeeleganz/src/main/java/com/kaffeeleganz/models/AppUserModel.java
+++ kaffeeleganz/src/main/java/com/kaffeeleganz/models/AppUserModel.java
@@ -13,19 +13,7 @@ public class AppUserModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
-    private String birthDate;
-    private String joinedDate;
     private String email;
-    private String address;
+    private String cpf;
     private String password;
-    // private String profilePicture;
-    // private String description;
-    // private String orderHistory;
-
-    // métodos:
-    // login;
-    // acessar perfil?;
-    // atualizar foto
-    // editar informações
-    // ver histórico de pedidos
 }
