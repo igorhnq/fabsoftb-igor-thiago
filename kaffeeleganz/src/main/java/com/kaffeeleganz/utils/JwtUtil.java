9b3ed55eed3a87f2e0c31e50e38cb0d74c0c90d2
igorhnq <igorhenrique102@gmail.com>
2025-05-05 21:10:51 -0300

    feat: adiciona arquivo de filtro de jwt

---
+++ kaffeeleganz/src/main/java/com/kaffeeleganz/utils/JwtUtil.java
@@ -0,0 +1,40 @@
+package com.kaffeeleganz.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.Claims;
+
+import java.util.Date;
+
+public class JwtUtil {
+    private static final String SECRET_KEY = "chave";
+
+    public static String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000)) 
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public static String extractEmail(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public static boolean isTokenValid(String token) {
+        try {
+            Claims claims = Jwts.parser()
+                    .setSigningKey(SECRET_KEY)
+                    .parseClaimsJws(token)
+                    .getBody();
+            return claims.getExpiration().after(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
