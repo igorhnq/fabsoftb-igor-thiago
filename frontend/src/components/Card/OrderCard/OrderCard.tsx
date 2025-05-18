9b24cf67a84c3053780561ed3f9f2b859a6dcd2e
igorhnq <igorhenrique102@gmail.com>
2025-05-18 12:35:35 -0300

    feat: adiciona propriedade de preço pro valor vir do backend

--- frontend/src/components/Card/OrderCard/OrderCard.tsx
+++ frontend/src/components/Card/OrderCard/OrderCard.tsx
@@ -1,13 +1,20 @@
 import styles from "./OrderCard.module.css";
 
-export default function OrderCard() {
+interface OrderCardProps {
+    order: {
+        orderDate: string;
+        totalAmount: number;
+    }
+}
+
+export default function OrderCard({ order }: OrderCardProps) {
     return (
         <div className={styles.orderCard}>
             <div className={styles.orderCardHeader}>
                 <h2 className={styles.orderCardTitle}>Kaffe Eleganz</h2>
                 <div className={styles.orderCardInfo}>
-                    <p>Data: 17/05/2025</p>
-                    <p>Total: R$ 10,00</p>
+                    <p>Data: {new Date(order.orderDate).toLocaleDateString()}</p>
+                    <p>Total: R$ {order.totalAmount.toFixed(2).replace('.', ',')}</p>
                 </div>
             </div>
         </div>
