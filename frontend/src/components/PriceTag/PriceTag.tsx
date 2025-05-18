ac6c09697963f33b9e17255431ce37ba0cc8dafa
igorhnq <igorhenrique102@gmail.com>
2025-05-18 14:06:54 -0300

    feat: adiciona propriedade de tamanho e cor de fundo

--- frontend/src/components/PriceTag/PriceTag.tsx
+++ frontend/src/components/PriceTag/PriceTag.tsx
@@ -3,12 +3,20 @@ import styles from './PriceTag.module.css';
 interface PriceTagProps {
     price: number;
     showTotalLabel?: boolean;
+    fontSize?: string;
+    backgroundColor?: string;
 }
 
-export default function PriceTag({ price, showTotalLabel }: PriceTagProps) {
+export default function PriceTag({ price, showTotalLabel, fontSize, backgroundColor }: PriceTagProps) {
     return (
         <div>
-            <h2 className={styles.priceTag}>
+            <h2
+                className={styles.priceTag}
+                style={{
+                    fontSize: fontSize,
+                    backgroundColor: backgroundColor
+                }}
+            >
                 {showTotalLabel && "Total "}
                 R$ {price.toFixed(2).replace('.', ',')}
             </h2>
